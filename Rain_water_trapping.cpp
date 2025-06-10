58528540b3a9a3d28aa1b1e85a3e46772d868be7
Suraj-kumar-pattnayak <surajkumarpattnayak@gmail.com>
2025-06-10 22:33:02 +0530

    goodprob

---
+++ Rain_water_trapping.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>arr = {3, 0, 0, 2, 0, 4};    //10
+    int n= arr.size();
+    vector<int>lmax;
+    int rmax[n];
+    vector<int>water;
+
+    lmax.push_back(arr[0]);
+    for(int i=1; i<n; i++)
+    {
+        lmax.push_back(max(lmax[i-1], arr[i]));
+    }
+
+    rmax[n-1] = arr[n-1];
+    for(int i= n-2; i>=0; i--)
+    {
+        rmax[i] = (max(rmax[i+1], arr[i]));
+    }
+    //water for each building
+    int sum =0;
+    for(int i=0; i<n; i++)
+    {
+        water.push_back(min(lmax[i] , rmax[i]) - arr[i]);
+        sum += water[i];
+    }
+
+    // int sum =0;
+    // for(int i=0; i<n; i++)
+    // {
+    //     sum += water[i];
+    // }
+    cout<<"ANS = "<<sum;
+    return 0;
+}
